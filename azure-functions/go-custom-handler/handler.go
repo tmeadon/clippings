28254d88811e70fda595322d3af4334491892760
tommagumma <tom@meadon.me>
2021-06-10 10:03:31 +0100

    add go custom handler function

---
+++ azure-functions/go-custom-handler/handler.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	message := "Function executed from Go.  Pass a name in for personalised response.\n"
+	name := r.URL.Query().Get("name")
+	if name != "" {
+		message = fmt.Sprintf("Hello %s", name)
+	}
+	fmt.Fprint(w, message)
+}
+
+func main() {
+	listenAddr := ":8080"
+	if val, ok := os.LookupEnv("FUNCTIONS_CUSTOMHANDLER_PORT"); ok {
+		listenAddr = ":" + val
+	}
+	http.HandleFunc("/api/HttpTrigger1", helloHandler)
+	log.Printf("About to listen on %s. Go to http://127.0.0.1%s/", listenAddr, listenAddr)
+	log.Fatal(http.ListenAndServe(listenAddr, nil))
+}
